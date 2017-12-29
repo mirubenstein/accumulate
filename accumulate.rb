9ebbd930d463bbc31fba1b771517d83f463afc0c
Mark R <mirubenstein@gmail.com>
2017-12-29 09:06:52 -0800

    Update enum_for to be correct. Base off of @remcopeereboom

--- accumulate.rb
+++ accumulate.rb
@@ -1,6 +1,6 @@
 class Array
   def accumulate
-    return enum_for { length } unless block_given?
+    return enum_for(__method__) { size } unless block_given?
     each_with_object([]) { |value, output| output << yield(value) }
   end
 end
